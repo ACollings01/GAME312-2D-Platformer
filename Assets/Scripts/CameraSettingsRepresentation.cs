33109b89d45c62204731c51df35ef22f2bbca7ad
ACollings01 <alexandercollings@protonmail.com>
2019-11-11 21:47:56 -0500

    Created basic classes for serializing and deserializing level data

---
+++ Assets/Scripts/CameraSettingsRepresentation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+[Serializable]
+public class CameraSettingsRepresentation
+{
+    public float cameraTrackTarget;
+    public float trackingSpeed;
+    public float cameraZDepth;
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+}
